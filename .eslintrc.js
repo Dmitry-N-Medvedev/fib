d079c8b596bcdfee408a95e59d2c5a46b1a232eb
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2019-10-10 12:59:35 +0200

    prepared project
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:node/recommended'
+  ],
+  plugins: [
+    'chai-friendly',
+  ],
+  rules: {
+    'no-unused-expressions': 0,
+    'chai-friendly/no-unused-expressions': 2,
+    'no-param-reassign': 0,
+    'camelcase': 0,
+    'node/exports-style': ['error', 'module.exports'],
+    'node/file-extension-in-import': ['error', 'always'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
+    'node/no-unpublished-require': 0,
+    'node/no-missing-require': 0,
+  },
+};
---
+++ specs/fib.spec.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+
+const {
+  describe,
+  it,
+} = require('mocha');
+const {
+  expect,
+} = require('chai');
+
+describe('Fibonacci Sequence', () => {
+  it('should calculate Nth position', () => {
+
+  });
+});
